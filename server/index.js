6fcdfa5a07d78d6d190df69c8f9b9dcb6f6d4c2c
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2025-07-19 09:56:55 +0530

    added databse

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 const app=express();
 const port=9901;
-app.listen(port,()=>
+const url="mongodb+srv://adnanahmed2578:WGfLQHotnvOjF7u8@cluster0.y4czkva.mongodb.net/";
+const connect=mongoose.connect(url)
+.then(()=>
+{
+    console.log("we are connected")
+    app.listen(port,()=>
 {
     console.log(` we are on port ${port}`)
 })
+})
+.catch((e)=>
+{
+    console.log(e)
+})
+ 
